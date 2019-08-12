29833a7e0b9e473bc9129fcdc1cc606df8a34e11
Iosif Takakura <takakura.yusuke@gmail.com>
2019-08-13 08:32:23 +0900

    split database settings.

--- huidetang/settings/base.py
+++ huidetang/settings/base.py
@@ -93,25 +93,6 @@ WSGI_APPLICATION = 'huidetang.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'huidetang',
-        'USER': os.getenv("DJANGO_STAGING_USER"),
-        'PASSWORD': os.getenv("DJANGO_STAGING_PASSWORD"),
-        'HOST': os.getenv("DJANGO_STAGING_HOST"),
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'connect_timeout': 28800,
-
-        },
-        'TEST': {
-            'NAME': 'test_huidetang'
-        }
-    }
-}
 
 
 # Password validation
--- huidetang/settings/production.py
+++ huidetang/settings/production.py
@@ -6,3 +6,24 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'huidetang',
+        'USER': os.getenv("DJANGO_STAGING_USER"),
+        'PASSWORD': os.getenv("DJANGO_STAGING_PASSWORD"),
+        'HOST': os.getenv("DJANGO_STAGING_HOST"),
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'connect_timeout': 28800,
+
+        },
+        'TEST': {
+            'NAME': 'test_huidetang'
+        }
+    }
+}
+
