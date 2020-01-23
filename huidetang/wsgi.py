5fc63d9006a5a67105d61d9b56822d170d8b3412
Iosif Takakura <takakura.yusuke@gmail.com>
2020-01-24 08:52:08 +0900

    setting test.

--- huidetang/wsgi.py
+++ huidetang/wsgi.py
@@ -12,5 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "huidetang.settings.localhost")
+import pymysql
+
+pymysql.install_as_MySQLdb()
+pymysql.version_info = (1, 3, 13, 'final', 0)
 
 application = get_wsgi_application()
