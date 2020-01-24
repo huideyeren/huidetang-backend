beb46a96454f9f6ee006efbabda735bb5ecb26bc
Iosif Takakura <takakura.yusuke@gmail.com>
2020-01-24 12:54:30 +0900

    add django.contrib.sites

--- huidetang/settings/base.py
+++ huidetang/settings/base.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'django_s3_storage',
     'django_ses',
