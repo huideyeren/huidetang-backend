f52df228fdeca7eb7bc92871d314967b2601019e
Iosif Takakura <takakura.yusuke@gmail.com>
2020-03-05 12:58:31 +0900

    updated

--- manage.py
+++ manage.py
@@ -7,9 +7,4 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    import pymysql
-
-    pymysql.install_as_MySQLdb()
-    pymysql.version_info = (1, 3, 13, 'final', 0)
-
     execute_from_command_line(sys.argv)
